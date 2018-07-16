04f7b960011ff7b2eea116e181332e186cfaa2f1
J. Holmes <jholmes@n.io>
2018-07-16 16:34:58 -0600

    adding block-type entry

--- stdlib/identity-interval-simulator.go
+++ stdlib/identity-interval-simulator.go
@@ -78,3 +78,112 @@ func (iis *IdentityIntervalSimulatorBlock) Start(ctx context.Context) {
 		}
 	}
 }
+
+var IdentityIntervalSimulator = nio.BlockTypeEntry{
+	Create:     nil,
+	Definition: nio.BlockTypeDefinition{
+		Version: "0.2.0",
+		BlockAttributes: nio.BlockAttributes{
+			Outputs: []nio.TerminalDefinition{
+				{
+					Label:       "default",
+					Type:        "output",
+					Visible:     true,
+					Order:       0,
+					ID:          "__default_terminal_value",
+					Default:     true,
+				},
+			},
+			Inputs: []nio.TerminalDefinition{},
+		},
+		Namespace: "goblocks.simulator.blocks.IdentityIntervalSimulator",
+		Properties: map[nio.Property]nio.PropertyDefinition{
+			"interval": {
+				"order": 0,
+				"type": "TimeDeltaType",
+				"advanced": false,
+				"visible": true,
+				"default": map[string]float64{
+					"seconds": 1,
+				},
+				"allow_none": false,
+				"title": "Interval",
+			},
+			"num_signals": {
+				"order": 3,
+				"type": "IntType",
+				"advanced": false,
+				"visible": true,
+				"default": 1,
+				"allow_none": false,
+				"title": "Number of Signals",
+			},
+			"total_signals": {
+				"order": 4,
+				"type": "IntType",
+				"advanced": false,
+				"visible": true,
+				"default": -1,
+				"allow_none": false,
+				"title": "Total Number of Signals",
+			},
+			"version": {
+				"order": nil,
+				"type": "StringType",
+				"advanced": true,
+				"visible": true,
+				"default": "0.2.0",
+				"allow_none": false,
+				"title": "Version",
+			},
+			"type": {
+				"order": nil,
+				"advanced": false,
+				"visible": false,
+				"title": "Type",
+				"type": "StringType",
+				"readonly": true,
+				"allow_none": false,
+				"default": nil,
+			},
+			"id": {
+				"order": nil,
+				"type": "StringType",
+				"advanced": false,
+				"visible": false,
+				"default": nil,
+				"allow_none": false,
+				"title": "Id",
+			},
+			"name": {
+				"order": nil,
+				"type": "StringType",
+				"advanced": false,
+				"visible": false,
+				"default": nil,
+				"allow_none": true,
+				"title": "Name",
+			},
+			"log_level": {
+				"order": nil,
+				"options": map[string]int{
+					"WARNING": 30,
+					"NOTSET": 0,
+					"ERROR": 40,
+					"INFO": 20,
+					"DEBUG": 10,
+					"CRITICAL": 50,
+				},
+				"advanced": true,
+				"visible": true,
+				"title": "Log Level",
+				"type": "SelectType",
+				"enum": "LogLevel",
+				"allow_none": false,
+				"default": "NOTSET",
+			},
+		},
+		Commands: map[nio.Command]nio.CommandDefinition{},
+		Name: "IdentityIntervalSimulator",
+	},
+}
